5f5719fe1665df68f30c837f5f9a19f5720fb948
sho_otb <shota715otobe@gmail.com>
2023-01-21 16:42:21 +0900

    “template”

---
+++ src/componrnts/BlogPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BlogPage = () => {
+  return (
+    <div className='container'>
+        <h1>ブログ</h1>
+
+        <p>lorem100</p>
+    </div>
+  )
+}
+
+export default BlogPage
